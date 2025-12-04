610bca8ca599cf7639683578e7f26592fec4934e
diazfacundom <diazfacundom@gmail.com>
2025-12-04 13:29:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ app.use('/', apiRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app; // exportamos para los tests
